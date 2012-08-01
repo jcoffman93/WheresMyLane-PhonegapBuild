2cdd1475ed846d6d7a14fd790702951880da7f8d
Jeremy <jcoffman93@yahoo.com>
2012-08-01 12:06:15 -0400

    changed url to test site

--- js/getMessages.js
+++ js/getMessages.js
@@ -1,12 +1,12 @@
 /* Grabs messages from server as JSON and puts them in ul#wrapper */
 $(document).ready(function() {
-  //var myScroll=new iScroll("wrapper");
-  //displayMessages();
+  var myScroll=new iScroll("wrapper");
+  displayMessages();
   //setInterval(function() {displayMessages()}, 10000);
 });
 
 function displayMessages() {
-	$.getJSON('http://www.wheresmylane.org/messages.json', function(data) {
+	$.getJSON('http://thawing-citadel-8225.herokuapp.com/messages/mobile_view.json', function(data) {
   		var items = [];
   		for(i=0; i<data.length; i++) {
   			items.push('<li>' + data[i]["content"] + '<div id=time><sub>' + data[i]["created_at"] + '</sub></div></li>');
