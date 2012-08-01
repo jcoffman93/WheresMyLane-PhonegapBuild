8da24f632c3260b565d184ee303b0a56cf8f79d6
Jeremy <jcoffman93@yahoo.com>
2012-08-01 12:00:52 -0400

    changed url to heroku test site: http://thawing-citadel-8225.herokuapp.com/

--- js/sendUserInfo.js
+++ js/sendUserInfo.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
 //     //$("#loading").style.visibility="hidden";
     $("#submit_button").click(function() {
         $.post(
-            "http://www.wheresmylane.org/users/mobile_create.json",
+            "http://thawing-citadel-8225.herokuapp.com/users/mobile_create.json",
             { "email": $("#email").val() },
             function(data) {
                 storage=window.localStorage;
-                storage.setItem("token", data["token"]);
-                storage.setItem("numReports", data["NumReports"]);
-                alert("A confirmation email has been sent to the address you provided. Any reports you send will be archived until we receive your confirmation.");
-                window.location="index.html";
+                alert(data["response"]);
+                if(data["token"]) {
+                    storage.setItem("token", data["token"]);
+                    storage.setItem("numReports", data["NumReports"]);
+                    window.location="index.html";
+                }
             }
         );
     });
