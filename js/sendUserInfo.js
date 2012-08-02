22641432717f14efde17d922aa442b45cf2eb4af
Jeremy <jcoffman93@yahoo.com>
2012-08-02 11:17:47 -0400

    Set server to wheresmylane.org

--- js/sendUserInfo.js
+++ js/sendUserInfo.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 //     //$("#loading").style.visibility="hidden";
     $("#submit_button").click(function() {
         $.post(
-            "http://thawing-citadel-8225.herokuapp.com/users/mobile_create.json",
+            "http://wheresmylane.org/users/mobile_create.json",
             { "email": $("#email").val() },
             function(data) {
                 storage=window.localStorage;
